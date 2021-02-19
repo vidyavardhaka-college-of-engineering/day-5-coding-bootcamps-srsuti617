ed285b09946fb6f087aacecd3de31ee628750513
srsuti617 <srustigourani617@gmail.com>
2021-02-19 11:25:32 +0000

    vowels count

--- Program_1.c
+++ Program_1.c
@@ -5,9 +5,23 @@
 int main()
 {
 	char str[10];
-	scanf("%s", str);
+  int i,count=0;
+	scanf("%s",&str);
+  for (i=0;str[i]!='\0';i++)
+  {
+    if(str [i]=='a'||
+    str [i]=='e'||
+    str[i]=='i'||
+    str[i]=='o'||
+    str[i]=='u')
+  
 
+  {
+count++;
+}
+  }
+printf("%d",count);
 	//Write your code here
-
+  
 	return 0;
 }
